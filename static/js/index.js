c51fc8740d1b1f63a3278d8edb2eaa6ac51e99b2
Fernando Zegada <ferzeg3420@gmail.com>
2021-03-11 23:50:49 -0400

    A lot of progress in refactoring by using a FSM.

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,18 @@
+const MAX_PLAYERS = 9;
+
 let app = {};
 
 let init = (app) => {
     app.data = {
         message: "buffering...",
         state: "init", 
-        right_answer: "",
+        right_answer: [],
+
+        player_guesses: [],
+        right_guesses: [],
+
+        show_cash_increment: false,
+        show_lives_decrement: false,
 
         board: [],
         players: [],
@@ -40,20 +48,60 @@ let init = (app) => {
     app.get_cards = () => {
         axios.get(get_cards_url)
              .then( (result) => {
-               console.log(`get_cards: ${result.data.board}`);
-               console.log(`get_cards: ${result.data.players}`);
                app.vue.board = app.vue.enumerate(result.data.board);
                app.vue.players = 
                  app.vue.enumerate(result.data.players);
-               console.log(`-board: ${app.vue.board}`);
-               console.log(`-players: ${app.vue.players}`);
              });
+    };
+
+    app.add_or_delete_guess(player_number) => {
+       if( player_number > 0 && player_number <= players.length ) {
+         player_guesses[player_number] = !player_guesses[player_number];
+       }
+    }
+
+    app.goto_load = () => {
+       app.vue.state = "load";
+       app.vue.get_cards(); 
+       // card-flipping animation goes here
+       app.vue.goto_guess();
+    };
+
+    // to enable the make veredict button
+    app.goto_get_guess = () => {
+       app.vue.state = "get_guess";
+    };
+    
+    app.goto_make_guess = () => {
+       app.vue.state = "make_guess";
+       guess = [];
+       for( let i = 1; i <= MAX_PLAYERS ) {
+         if( app.vue.player_guesses[i] ) {
+           guess.append(i);
+         }
+       }
+       app.vue.post_guess(guess);
+       app.vue.goto_compare();
+    };
+    
+    // used to enable the continue button
+    // and show -1 to lives or +5 to $
+    app.goto_compare = () => {
+       app.vue.state = "compare";
+       for( let i of app.vue.right_answer ) {
+           app.vue.right_guesses[i] = true;
+       }
     };
 
     app.methods = {
         enumerate: app.enumerate,
         post_guess: app.post_guess,
         get_cards: app.get_cards,
+        goto_load: app.goto_load, 
+        goto_get_guess: app.goto_get_guess, 
+        goto_make_guess: app.goto_make_guess, 
+        goto_compare: app.goto_compare, 
+        add_or_delete_guess: app.add_or_delete_guess,
     };
 
     app.vue = new Vue({
@@ -64,7 +112,11 @@ let init = (app) => {
 
     app.init = () => {
        console.log("initing");
-       app.vue.get_cards(); 
+       app.vue.player_guesses = [];
+       for( let i = 0; i <= MAX_PLAYERS; i++) {
+           app.vue.player_guesses.append(false);
+       }
+       app.vue.goto_load();
     };
 
     app.init();
